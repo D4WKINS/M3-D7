44ffa8dfb85bcf0572b0b2a38bd2f1718072115e
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-05-26 04:41:50 +0000

    Update M3-D7

--- HW.js
+++ HW.js
@@ -1,34 +1,53 @@
 window.onload = () =>{
     generateUsers()
   }
+
+
   const generateUsers = async () =>{
     try{
         const response = await fetch ("https://jsonplaceholder.typicode.com/users" )
         const data = await response.json()
-        const userTable = document.getElementBydId("user-details")
-       data.forEach(obj=>{
+        const userTable = document.getElementById("user-details")
 
-        <tr>
-        <th scope="row">obj.id</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
+        data.forEach(obj=>{
+           userTable.innerHTML+= `<tr>
+                        <th scope="row">${obj.id}</th>
+                        <td>${obj.name}</td>
+                        <td>${obj.username}</td>
+                        <td>${obj.email}</td>
+                        <td>${obj.phone}</td>
+                        <td>${obj.address.street}<br>${obj.address.suite}</td>
+                        <td>${obj.address.zipcode}</td>
+                        </tr>`
        }) 
 
-        return console.log(data)
-           
-        
+       filterSearch(data)
+
+     
     }
     catch(err){
         console.log("Houston we have problem", err)
     }
   }
 
-function getDropDownValue(e){
+
+function filterSearch(data){
+    /** Get drop down selected Value  */
     let selected =""
     const dropdownBtn = document.getElementById("dropdownbtn")
-      selected = e.target.value
-      dropdownBtn.innerText = selected
-      console.log(selected)
+    const dropdownItems = document.querySelectorAll(".dropdown-item")
+
+    dropdownItems.forEach(item =>{
+    item.onclick = (e) =>{
+            selected = e.target.value
+            dropdownBtn.innerText = selected
+            console.log(selected)
+    }})
+    /*--------------------------*/
+
+    const searchInput = document.getElementById("search-input")
+    searchInput.onkeyup = (e) =>{
+        console.log(e.target.value)
+    }
+  
 }
